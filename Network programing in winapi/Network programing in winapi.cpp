a9c6099c299afdc1da54b3d7765b9f825a8d78ba
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2024-01-28 19:06:44 +0300

    Add files via upload

---
+++ Network programing in winapi/Network programing in winapi.cpp
@@ -0,0 +1,35 @@
+﻿#define _WIN32_WINNT 0x600
+#include <iostream>
+#include <iomanip>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+#include <windows.h>
+using namespace std;
+int main()
+{
+    WSADATA wsadata;
+    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) throw runtime_error("WSAStartup failed");
+    SOCKET listening = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listening == INVALID_SOCKET) throw runtime_error("socket() failed");
+    sockaddr_in sock_in;
+    sock_in.sin_family = AF_INET;
+    inet_pton(AF_INET, "192.168.1.130", &sock_in.sin_addr);
+    sock_in.sin_port = htons(2024);
+    if (bind(listening, (sockaddr*)&sock_in, sizeof(sock_in)) != 0) throw runtime_error("bind server");
+    const int queue = 5;
+    if (listen(listening, queue) == SOCKET_ERROR) throw runtime_error("listen failed");
+    cout << "listening on port" << 2024 << endl;
+    while (true)
+    {
+        SOCKET socket_ = accept(listening, NULL, NULL);
+        if (socket_ == INVALID_SOCKET) throw runtime_error("accept failed");
+    }
+    SOCKET socket;
+    if (connect(socket, (sockaddr*)&sock_in, sizeof(sock_in)) == SOCKET_ERROR) throw runtime_error("connect failed");
+    char buffer[4];
+    recv(socket, buffer, 4,
+        0);
+    byte length = *(reinterpret_cast<byte*>(buffer));
+    if (length % 1024 == 0) cout << "#";
+}
---
+++ Network programing/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Network_programing
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static string file_cont = string.Empty,
+            file_path = string.Empty;
+        private void file_Click(object sender, RoutedEventArgs e)
+        {
+            new OpenFileDialog().InitialDirectory = "";
+            new OpenFileDialog().Filter = "";
+            new OpenFileDialog().RestoreDirectory = true;
+            if (new OpenFileDialog().ShowDialog() == true)
+            {
+                file_path = new OpenFileDialog().FileName;
+                file_cont = new StreamReader(new OpenFileDialog().OpenFile()).ReadToEnd();
+                byte[] infobuffer = new byte[new MemoryStream().Length];
+                new MemoryStream().Close();
+                new FileStream(file_path, FileMode.Open, FileAccess.Read).Read(infobuffer, infobuffer.Length, (int)new FileInfo(file_path).Length);
+                new FileStream(file_path, FileMode.Open, FileAccess.Read).Close();
+                new TcpClient("192.168.1.130", 2024).GetStream().Write(infobuffer, 0, infobuffer.Length);
+            }
+        }
+    }
+}
